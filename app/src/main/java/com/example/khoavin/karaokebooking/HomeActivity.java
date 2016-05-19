188bcb6d65982541ad4906f78e4f2ec53522dc66
thinhlv <12520417@gm.uit.edu.vn>
2016-05-20 01:24:29 +0700

    add simple map, long touch to set start and end point

--- app/src/main/java/com/example/khoavin/karaokebooking/HomeActivity.java
+++ app/src/main/java/com/example/khoavin/karaokebooking/HomeActivity.java
@@ -3,14 +3,11 @@ package com.example.khoavin.karaokebooking;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.provider.CalendarContract;
-import android.provider.SyncStateContract;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -18,9 +15,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.example.khoavin.karaokebooking.Adapter.PD_Adapter;
 import com.example.khoavin.karaokebooking.KaraokeObject.Phong_Dat;
@@ -173,7 +170,8 @@ public class HomeActivity extends AppCompatActivity
         } else if (id == R.id.nav_foods) {
 
         } else if (id == R.id.nav_maps) {
-
+            Intent it = new Intent(HomeActivity.this, MapActivity.class);
+            startActivity(it);
         } else if (id == R.id.nav_kara) {
             Intent it = new Intent(HomeActivity.this, KaraokeActivity.class);
             startActivity(it);
